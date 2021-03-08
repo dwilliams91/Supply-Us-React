e5ac476f4c77cfda51f01b2430e267d625390338
Your Github User Name <david.lav.williams@gmail.com>
2021-03-08 09:06:43 -0600

    the login works when a user exists and shows the model when the user doesn't exist

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -31,6 +31,7 @@ export const Login = props => {
                     history.push("/teachers")
                 }
                 else {
+                    console.log("hi")
                     invalidDialog.current.showModal()
                 }
             })
