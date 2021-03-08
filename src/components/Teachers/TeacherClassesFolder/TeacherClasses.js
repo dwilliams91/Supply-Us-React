181aa8055f415ccaff631dd3e8253a63fb00ba92
Your Github User Name <david.lav.williams@gmail.com>
2021-03-08 10:44:38 -0600

    a teachers classes are displayed when they login

--- src/components/Teachers/TeacherClassesFolder/TeacherClasses.js
+++ src/components/Teachers/TeacherClassesFolder/TeacherClasses.js
@@ -11,32 +11,20 @@ export const TeacherClasses = () => {
         getClassLists()
     }, [])
 
-    // Goes through all the classes and returns only the ones with the userId that is the same as the logged in user
-    const filterClasses = (allClasses) => {
-        const user = parseInt(localStorage.getItem("app_user_id"))
-        const userType=parseInt(localStorage.getItem("userType"))
-        if (userType===1){
-            const myClasses = allClasses.filter(singleClass => parseInt(singleClass.userId) === user)
-            return myClasses
-        } else if (userType===3){
-            const myClasses=allClasses
-            return myClasses
-        }
-        // console.log(myClasses)
-    }
+    
 
     return (
         <>
-            {/* <div className="h1Background">
+            <div className="h1Background">
             <h1 >My Classes</h1>
             </div>
             <div className="classCardContainer">
                 <div className="classCardFlex">
-                    {filterClasses(classLists).map(singleClass => {
+                    {classLists.map(singleClass => {
                         return <TeacherClass key={singleClass.id} myClass={singleClass}></TeacherClass>
                     })}
                 </div>
-            </div> */}
+            </div>
         </>
     )
 
